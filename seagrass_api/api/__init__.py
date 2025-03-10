93928978449a7c2a74124de7502ad7c7a7230181
Gilles <autiergilles@gmail.com>
2025-03-10 12:38:47 +0100

    Update structure for project

--- seagrass_api/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from .fast import router as fast_router
-
-api_router = APIRouter()
-
-api_router.include_router(fast_router)
-# api_router.include_router(fast_router, prefix="/users", tags=["Users"])
