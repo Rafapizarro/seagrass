2b0a28d7be60da7c853feceec2d894ce14cbe59d
JP Alvarez <jpabalto11@gmail.com>
2025-03-14 20:06:42 +0100

    commit before pulling

--- seagrass/ml_logic/load_data.py
+++ seagrass/ml_logic/load_data.py
@@ -3,12 +3,11 @@ import pandas as pd
 import geopandas as gpd
 from shapely import wkt
 from pathlib import Path
-from seagrass.params import *
+from seagrass.params import GCP_PROJECT, BQ_DATASET
 from google.cloud import bigquery
 
-def load_features(
-    cache_path:Path,
-    limit=None) -> gpd.GeoDataFrame:
+
+def load_features(cache_path: Path, limit=None) -> gpd.GeoDataFrame:
     """
     Load features data from local cache or BigQuery.
 
@@ -22,15 +21,15 @@ def load_features(
     Returns
     -------
     GeoDataFrame
-        Features data with CRS set to EPSG:4326.
+        Features data with CRS set to EPSG:32633.
     """
 
     if Path(cache_path).is_file():
-        print("\nLoad data from local Parquet file...")
+        print("\nLoad feature data from local Parquet file...")
         data = pd.read_parquet(cache_path)
 
     else:
-        print("\nLoad data from BigQuery server...")
+        print("\nLoad feature data from BigQuery server...")
         client = bigquery.Client()
         query = f"""
         SELECT *
@@ -41,14 +40,14 @@ def load_features(
         data = client.query(query).to_dataframe()
         data.to_parquet(cache_path)
 
-    gdf = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.lon, data.lat), crs="EPSG:4326")
+    gdf = gpd.GeoDataFrame(
+        data, geometry=gpd.points_from_xy(data.lon, data.lat), crs="EPSG:32633"
+    )
 
     return gdf
 
 
-def load_targets(
-    cache_path:Path,
-    limit=None) -> gpd.GeoDataFrame:
+def load_targets(cache_path: Path, limit=None) -> gpd.GeoDataFrame:
     """
     Load target data from local cache or BigQuery.
 
@@ -66,11 +65,11 @@ def load_targets(
     """
 
     if Path(cache_path).is_file():
-        print("\nLoad data from local Parquet file...")
+        print("\nLoad target data from local Parquet file...\n")
         data = pd.read_parquet(cache_path)
 
     else:
-        print("\nLoad data from BigQuery server...")
+        print("\nLoad target data from BigQuery server...\n")
         client = bigquery.Client()
         query = f"""
         SELECT *
@@ -82,26 +81,29 @@ def load_targets(
         data.to_parquet(cache_path)
 
     data["coordinates"] = data["coordinates"].apply(wkt.loads)
-    gdf = gpd.GeoDataFrame(data, geometry="coordinates", crs="EPSG:4326")
+    gdf = gpd.GeoDataFrame(data, geometry="coordinates", crs="EPSG:32633")
 
     return gdf
 
+
 def merge_data(
+    cache_path: Path,
     features: gpd.GeoDataFrame,
     targets: gpd.GeoDataFrame,
-    max_distance:int) -> gpd.GeoDataFrame:
+    max_distance: int,
+) -> gpd.GeoDataFrame:
     """
     Merge feature and target GeoDataFrames using a spatial nearest join.
 
     Parameters
     ----------
     features : gpd.GeoDataFrame
-        Feature data with CRS set to EEPSG:3035.
+        Feature data with CRS set to EPSG:32633.
     targets : gpd.GeoDataFrame
-        Target data with CRS set to EEPSG:3035.
+        Target data with CRS set to EPSG:32633.
     max_distance : float
         Maximum distance (in CRS units) allowed between polygons and points for joining.
-        For EEPSG:3035, distance is measured in meters (e.g., 0.01 degrees = ~1 km).
+        For EPSG:32633, distance is measured in meters (e.g., 0.01 degrees = ~1 km).
 
     Returns
     -------
@@ -109,13 +111,9 @@ def merge_data(
         Merged GeoDataFrame resulting from a left spatial join.
     """
 
-    print("\nMerging files...")
-    df = gpd.sjoin_nearest(features, targets, how="left",max_distance=max_distance)
-    print("\nFiles merged!")
+    print("\nMerging files...\n")
+    df = gpd.sjoin_nearest(features, targets, how="left", max_distance=max_distance)
+    df.to_parquet(cache_path)
+    print("\nMerge complete.\n")
 
     return df
-
-if __name__ == "__main__":
-    features = load_features(cache_path=os.path.join(f"{LOCAL_DATA_PATH}",f"{BQ_DATASET}_features.parquet"))
-    targets = load_targets(cache_path=os.path.join(f"{LOCAL_DATA_PATH}",f"{BQ_DATASET}_targets.parquet"))
-    df = merge_data(features,targets)
